06feff9d5e52f98fef323f8ab46c801e9835a2a5
fernetsystem <fernetsystem@gmail.com>
2023-05-12 01:27:13 -0600

    Creare microservice compras

---
+++ src/main/java/com/visys/compras/VisysComprasApplication.java
@@ -0,0 +1,13 @@
+package com.visys.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisysComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisysComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visys/compras/VisysComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visys.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisysComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
