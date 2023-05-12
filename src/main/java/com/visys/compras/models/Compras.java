42fb41c682add896af36a8c7a3c31046cafe286b
fernetsystem <fernetsystem@gmail.com>
2023-05-12 01:45:09 -0600

    Mapping bean table compras

---
+++ src/main/java/com/visys/compras/models/Compras.java
@@ -0,0 +1,32 @@
+package com.visys.compras.models;
+
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "compra")
+public class Compras {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "inmueble_id", nullable = false)
+    private Long inmuebleId;
+
+    @Column(name = "tittle")
+    private String tittle;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "dateBuy", nullable = false)
+    private LocalDateTime dateBuy;
+}
---
+++ src/main/java/com/visys/compras/repository/CompraRepository.java
@@ -0,0 +1,2 @@
+package com.visys.compras.repository;public interface CompraRepository {
+}
